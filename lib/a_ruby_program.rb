c0f14218fb8d6ae6239afab28801291151dc9e1d
sgg2123 <sgg2123@columbia.edu>
2018-04-19 04:15:21 +0000

    automatically backed up by learn

--- lib/a_ruby_program.rb
+++ lib/a_ruby_program.rb
@@ -20,4 +20,4 @@ puts "Finally, barewords, or variables, that are set equal to things."
 
 puts "Anything that isn't one of those will cause an error."
 
-puts "See, I fixed it."
+puts "See, I fixed the error."
